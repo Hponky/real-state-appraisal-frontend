5093b43959241edc947a67310ebf973d648b5550
Humberto <humberto228xd@gmail.com>
2025-06-12 19:01:54 -0500

    supabase urls fix

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = "https://uvwfemunxdyxhvpflfqv.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV2d2ZlbXVueGR5eGh2cGZsZnF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIwOTY4NzIsImV4cCI6MjA1NzY3Mjg3Mn0.iF2oVBm8pZ-W81oDya3JGBJhRa4YjvItRdc7AQSRiEY";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
